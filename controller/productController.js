96d4b678c7ce695172ede3da240c53497583ed89
Adam Lang <adam.lang-1@hotmail.com>
2021-04-18 15:04:42 +0200

    fix: removed console.log

--- controller/productController.js
+++ controller/productController.js
@@ -1,9 +1,7 @@
 const { products } = require('../products');
 
 const getProducts = (req, res) => {
-    // console.log(req.name)
-    const newProduct = products.map((product) => product.name)
-    res.status(200).json({ success: true, data: newProduct });
+    res.status(200).send(products);
 };
 
 module.exports = { getProducts };
