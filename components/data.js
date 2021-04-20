b54b36e32e813e7929293065aa2e90872b9f025c
Adam Lang <adam.lang-1@hotmail.com>
2021-04-20 22:52:32 +0200

    feat: rerouted laptop url correctly to frontend

--- components/data.js
+++ components/data.js
@@ -1,3 +1,4 @@
+// MAIN CATEGORIES
 const categories = [
     {
         id: 1,
@@ -6,7 +7,7 @@ const categories = [
             'https://images.unsplash.com/photo-1507764923504-cd90bf7da772?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
         price: 39.95,
         desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
-        link: 'sub-categories',
+        link: 'laptops',
     },
     {
         id: 2,
@@ -24,7 +25,7 @@ const categories = [
             'https://images.unsplash.com/photo-1555680202-c86f0e12f086?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         price: 10.99,
         desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
-        link: 'sub-categories',
+        link: 'components',
     },
     {
         id: 4,
@@ -33,14 +34,15 @@ const categories = [
             'https://images.unsplash.com/photo-1594063788908-2ee05db0ec48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80',
         price: 9.99,
         desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
-        link: 'sub-categories',
+        link: 'accessories',
     },
 ];
 
+// LAPTOPS
 const laptops = [
     {
         id: 5,
-        name: 'Macbook',
+        name: 'Apple',
         image:
             'https://images.unsplash.com/photo-1494498902093-87f291949d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         price: `$1499`,
@@ -53,8 +55,9 @@ const laptops = [
         image:
             'https://images.unsplash.com/photo-1536508183722-0f11d94c62c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         price: `$1299`,
-        desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
-        It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
+        desc: `Chromebook is a computer built by Google. Chromebook is perfect when you are in need of a laptop for studies or work
+        on-the-go. It has built in virus protection software, battery that lasts for a whole day and the best function from Google
+        built in.`,
     },
     {
         id: 7,
@@ -62,8 +65,9 @@ const laptops = [
         image:
             'https://images.unsplash.com/photo-1551033541-2075d8363c66?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
         price: `$999`,
-        desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
-        It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
+        desc: `In this category you will find your new gaming laptop. With laptops from ASUS, MSI and Lenovo, we will guarantee that
+        you will find the perfect match for your needs. If you want the best gaming experience, we recommend that you consider a product 
+        with a screen that has an updating frequence of 120 to 144Mhz.`,
     },
     {
         id: 8,
@@ -71,14 +75,14 @@ const laptops = [
         image:
             'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         price: `$349`,
-        desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
-        It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
+        desc: `Dell's new two-in-one laptops are manufactured with precision and premium material and has screens with a natural
+        image to give you the best possible experience. We have the performance that you need.`,
     },
     {
         id: 9,
         name: 'Asus',
         image:
-            'https://images.unsplash.com/photo-1494498902093-87f291949d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1582203422342-1541a90a0103?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         price: `$639`,
         desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
         It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
@@ -87,7 +91,7 @@ const laptops = [
         id: 10,
         name: 'Lenovo',
         image:
-            'https://images.unsplash.com/photo-1536508183722-0f11d94c62c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1578598335941-2ddafa759de2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
         price: `$899`,
         desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
         It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
@@ -96,20 +100,20 @@ const laptops = [
         id: 11,
         name: 'HP',
         image:
-            'https://images.unsplash.com/photo-1551033541-2075d8363c66?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+            'https://images.unsplash.com/photo-1589561084283-930aa7b1ce50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
         price: `$829`,
         desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
         It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
     },
     {
         id: 12,
-        name: 'Samsung',
+        name: 'Acer',
         image:
-            'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1525971977907-20d22da82d6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1260&q=80',
         price: `$549`,
         desc: `Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels.
         It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.`,
     },
 ];
-
+// LAPTOP BRAND
 module.exports = { categories, laptops };
